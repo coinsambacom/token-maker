733ff95a42e39e0fb5be1f3246556ba410517648
Gustavo <gustavo@obex.pw>
2023-05-18 07:17:04 -0300

    checkpoint

--- contracts/MintableERC20.sol
+++ contracts/MintableERC20.sol
@@ -7,14 +7,18 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
-contract StandardERC20 is ERC20Upgradeable {
+contract MintableERC20 is ERC20 {
     function initialize(
         address owner_,
         string memory name_,
         string memory symbol_,
-        uint256 supply_,
+        uint256 supply_
     ) external initializer {
         __ERC20_init(name_, symbol_);
         _mint(owner_, supply_);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
