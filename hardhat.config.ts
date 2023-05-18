c63088851b32fec8090af551a4e5d037521d5b30
Gustavo <gustavo@obex.pw>
2023-05-18 10:12:03 -0300

    checkpoint

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,8 +6,29 @@ const config: HardhatUserConfig = {
   networks: {
     dashboard: {
       url: "http://localhost:24012/rpc",
-      gasPrice: 50000000000
+      gasPrice: 50000000000,
     },
+  },
+  etherscan: {
+    apiKey: "REPLACEME",
+    customChains: [
+      {
+        network: "alfajores",
+        chainId: 44787,
+        urls: {
+          apiURL: "https://api-alfajores.celoscan.io/api",
+          browserURL: "https://alfajores.celoscan.io",
+        },
+      },
+      {
+        network: "celo",
+        chainId: 42220,
+        urls: {
+          apiURL: "https://api.celoscan.io/api",
+          browserURL: "https://celoscan.io/",
+        },
+      },
+    ],
   },
 };
 
